c252d1b0526bb73fd13162054c8c71c8820084ae
Pilco <mirco.pilco@interfase.com.bo>
2025-06-20 15:59:59 -0400

    Init

---
+++ src/main/java/com/enterprise/project/demo1springboot/Demo1SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.enterprise.project.demo1springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1SpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/enterprise/project/demo1springboot/Demo1SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterprise.project.demo1springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
